df9f03317ba557d4ab77ecd6e160e3802775a600
zeqk.net <zeqk.net@e596edee-0f2f-11df-ae15-f54ea2efe8cf>
2010-02-01 16:09:49 +0000

    projecto zeqktools

---
+++ zeqktools/ZeqkTools/Data/Enumerators/DataProviders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZeqkTools.Data
+{
+    public enum DataProviders
+    {
+        OleDb,
+        Odbc,
+        SQLServer
+    }
+}
