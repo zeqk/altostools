5caf042efaadfac8efcf68f0d83b64e35ef34963
zeqk.net <zeqk.net@e596edee-0f2f-11df-ae15-f54ea2efe8cf>
2010-10-14 18:43:20 +0000

    - nuevas dll - test de Polygon

--- trunk/zeqktools/AltosTools.MapsTest/Form1.Designer.cs
+++ trunk/zeqktools/AltosTools.MapsTest/Form1.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(67, 129);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(98, 61);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Polygon form without marks";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(219, 129);
+            this.button2.Location = new System.Drawing.Point(37, 41);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(98, 61);
             this.button2.TabIndex = 1;
@@ -55,9 +44,9 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(67, 227);
+            this.button3.Location = new System.Drawing.Point(210, 41);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(98, 42);
+            this.button3.Size = new System.Drawing.Size(98, 61);
             this.button3.TabIndex = 2;
             this.button3.Text = "Point";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,19 +56,17 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(405, 302);
+            this.ClientSize = new System.Drawing.Size(353, 149);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Demo";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
