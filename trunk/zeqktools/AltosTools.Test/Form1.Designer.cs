a2470192377f28832fd541fddf4686f254d20db9
zeqk.net <zeqk.net@e596edee-0f2f-11df-ae15-f54ea2efe8cf>
2010-10-14 13:47:27 +0000

    fix para el ExtendedCheckedListBox q para q no cargue 2 veces el item "(All)", lo mismo para CheckedListComboBox

--- trunk/zeqktools/AltosTools.Test/Form1.Designer.cs
+++ trunk/zeqktools/AltosTools.Test/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.extendedCheckedListBox1 = new AltosTools.WindowsForms.Controls.ExtendedCheckedListBox(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -55,17 +56,29 @@
             // extendedCheckedListBox1
             // 
             this.extendedCheckedListBox1.CheckOnClick = true;
+            this.extendedCheckedListBox1.DataSource = null;
             this.extendedCheckedListBox1.FormattingEnabled = true;
             this.extendedCheckedListBox1.Location = new System.Drawing.Point(81, 114);
             this.extendedCheckedListBox1.Name = "extendedCheckedListBox1";
             this.extendedCheckedListBox1.Size = new System.Drawing.Size(208, 184);
             this.extendedCheckedListBox1.TabIndex = 5;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(388, 137);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(554, 394);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.extendedCheckedListBox1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button1);
@@ -83,5 +96,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private AltosTools.WindowsForms.Controls.ExtendedCheckedListBox extendedCheckedListBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
